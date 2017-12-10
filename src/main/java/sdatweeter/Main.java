0d0d825da02cdd274f88fb74fffb3218df33ac46
PawelOrzel <pawel.orlowski87@wp.pl>
2017-12-10 12:10:49 +0100

    add setTimeStamp method in Main

--- src/main/java/sdatweeter/Main.java
+++ src/main/java/sdatweeter/Main.java
@@ -2,9 +2,11 @@ package sdatweeter;
 
 
 import org.hibernate.Session;
+import org.hibernate.type.TimestampType;
 import sdatweeter.model.MyTweet;
 import sdatweeter.model.User;
 import sdatweeter.utility.HibernateUtil;
+
 
 public class Main {
 
@@ -14,18 +16,19 @@ public class Main {
         Session session = HibernateUtil.getHibernateSession();
 
         User user1 = new User();
+        MyTweet myTweet1 = new MyTweet();
+
         user1.setNick("Janek");
         user1.setPass("Klocek");
 
-        MyTweet myTweet = new MyTweet();
-
-        myTweet.setMessage("Ala ma kota");
+        myTweet1.setMessage("Ala ma kota");
+        myTweet1.setTimestamp(System.currentTimeMillis());
 
         session.beginTransaction();
         session.save(user1);
 
-        myTweet.setUser(user1);
-        session.save(myTweet);
+        myTweet1.setUser(user1);
+        session.save(myTweet1);
 
         session.getTransaction().commit();
 
--- src/main/java/sdatweeter/model/User.java
+++ src/main/java/sdatweeter/model/User.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Entity
-@Table (name = "USER_DETAILS")
+@Table (name = "USERS")
 public class User {
 
     @Id
